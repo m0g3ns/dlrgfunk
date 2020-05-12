05113bb4c1c6bd7f9774139424e007dd1b5f65f0
m0g3ns <33782561+m0g3ns@users.noreply.github.com>
2020-05-12 18:09:03 +0200

    Updates

---
+++ precache-manifest.33e6cfcf93ad96268f5fa81621510130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2dc2c9c5192a336638157de96aebe4",
+    "url": "/dlrgfunk/index.html"
+  },
+  {
+    "revision": "139697ca464b65e3c783",
+    "url": "/dlrgfunk/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43ed6cb006294ea3997e",
+    "url": "/dlrgfunk/static/js/2.1e4e878f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dlrgfunk/static/js/2.1e4e878f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139697ca464b65e3c783",
+    "url": "/dlrgfunk/static/js/main.e1c50607.chunk.js"
+  },
+  {
+    "revision": "53dfc1980e40deba63aa",
+    "url": "/dlrgfunk/static/js/runtime-main.20c76c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dlrgfunk/static/media/logo.5d5d9eef.svg"
+  }
+]);
